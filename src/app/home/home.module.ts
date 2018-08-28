69ddc437b3f58cfd39a5e65af7e57a3454eb60ad
Vitor <metroid.p2p@gmail.com>
2018-08-28 13:25:41 -0300

    starting module 2

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { SigninComponent } from './signin/signin.component';
+import { VmessageModule } from '../shared/component/vmessage/vmessage.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    VmessageModule
+  ],
+  declarations: [
+    SigninComponent
+  ]
+})
+export class HomeModule { }
---
+++ src/app/shared/component/vmessage/vmessage.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-vmessage',
+  templateUrl: './vmessage.component.html',
+  styleUrls: ['./vmessage.component.css']
+})
+export class VmessageComponent implements OnInit {
+  @Input() text = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
